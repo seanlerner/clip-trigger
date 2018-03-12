ed13875eba5c5d841af2041a9f8e65208d0f2088
Sean Lerner <sean@@smallcity.ca>
2018-03-12 10:34:39 -0400

    tests for global log fn

--- tests/main/globals.js
+++ tests/main/globals.js
@@ -1,16 +1,67 @@
-// Node
-child_process = require('child_process')
-fs            = require('fs')
+describe('global.js', function () {
 
-// Packages
-chalk         = require('chalk')
+  const
+    actual   = last_log_line,
+    test_str = 'Testing 123'
 
-// Helper
-Helper        = require('./helper')
-helper        = new Helper
+  it('log()', () => {
+    log(test_str)
+    const expected = /\[info\] Testing 123/
+    assert(actual().match(expected), assert_fail_msg(expected, actual()))
+  })
 
-// Test Files
-TestsLoader   = require('./tests_loader')
-RunTest       = require('./run_test')
-RunTests      = require('./run_tests')
+  it('log.error()', () => {
+    log.error(test_str)
+    const expected = /\[error\] Testing 123/
+    assert(actual().match(expected), assert_fail_msg(expected, actual()))
+  })
 
+  it('log.warn()', () => {
+    log.warn(test_str)
+    const expected = /\[warn\] Testing 123/
+    assert(actual().match(expected), assert_fail_msg(expected, actual()))
+  })
+
+  it('log.verbose()', () => {
+    log.verbose(test_str)
+    const expected = /\[verbose\] Testing 123/
+    assert(actual().match(expected), assert_fail_msg(expected, actual()))
+  })
+
+  it('log.info()', () => {
+    log(test_str)
+    const expected = /\[info\] Testing 123/
+    assert(actual().match(expected), assert_fail_msg(expected, actual()))
+  })
+
+  it('log.debug()', () => {
+    log.debug(test_str)
+    const expected = /\[debug\] Testing 123/
+    assert(actual().match(expected), assert_fail_msg(expected, actual()))
+  })
+
+  it('log.silly()', () => {
+    log.silly(test_str)
+    const expected = /\[silly\] Testing 123/
+    assert(actual().match(expected), assert_fail_msg(expected, actual()))
+  })
+
+})
+
+function assert_fail_msg(expected, actual) {
+  return `
+      Expected: '${expected}'
+        to be found within:
+      Actual:   '${actual}'
+  `
+}
+
+function last_log_line() {
+  const
+    log_contents =
+      fs
+      .readFileSync(CT.vendor.electron_log.transports.file.findLogPath(), 'utf-8')
+      .split('\n')
+
+  return log_contents[log_contents.length - 2]
+}
---
+++ tests/test_helper.js
@@ -0,0 +1,4 @@
+assert = require('assert')
+require('../index.js')
+new CT.Ready()
+
