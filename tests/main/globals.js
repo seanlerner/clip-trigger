44aa31cbfa07536e0e6c048fd898a0ad8acb7ca3
Sean Lerner <sean@@smallcity.ca>
2018-03-12 11:13:16 -0400

    tests all global helpers

--- tests/main/globals.js
+++ tests/main/globals.js
@@ -1,67 +1,27 @@
 describe('global.js', function () {
 
-  const
-    actual   = last_log_line,
-    test_str = 'Testing 123'
-
-  it('log()', () => {
-    log(test_str)
-    const expected = /\[info\] Testing 123/
-    assert(actual().match(expected), assert_fail_msg(expected, actual()))
+  it('clip_trigger_credentials()', () => {
+    const expected = ['email', 'clip_trigger_token']
+    const actual   = Object.keys(clip_trigger_credentials())
+    assert.deepEqual(expected, actual)
   })
 
-  it('log.error()', () => {
-    log.error(test_str)
-    const expected = /\[error\] Testing 123/
-    assert(actual().match(expected), assert_fail_msg(expected, actual()))
+  it('single_line_str()', () => {
+    const expected = '{"abc":123}'
+    const actual   = single_line_str({ abc: 123 })
+    assert.equal(expected, actual)
   })
 
-  it('log.warn()', () => {
-    log.warn(test_str)
-    const expected = /\[warn\] Testing 123/
-    assert(actual().match(expected), assert_fail_msg(expected, actual()))
+  it('is_json() with valid json str', () => {
+    const expected = { "abc": 123 }
+    const actual   = is_json('{ "abc": 123 }')
+    assert.deepEqual(expected, actual)
   })
 
-  it('log.verbose()', () => {
-    log.verbose(test_str)
-    const expected = /\[verbose\] Testing 123/
-    assert(actual().match(expected), assert_fail_msg(expected, actual()))
-  })
-
-  it('log.info()', () => {
-    log(test_str)
-    const expected = /\[info\] Testing 123/
-    assert(actual().match(expected), assert_fail_msg(expected, actual()))
-  })
-
-  it('log.debug()', () => {
-    log.debug(test_str)
-    const expected = /\[debug\] Testing 123/
-    assert(actual().match(expected), assert_fail_msg(expected, actual()))
-  })
-
-  it('log.silly()', () => {
-    log.silly(test_str)
-    const expected = /\[silly\] Testing 123/
-    assert(actual().match(expected), assert_fail_msg(expected, actual()))
+  it('is_json() with invalid json str', () => {
+    const expected = undefined
+    const actual   = is_json('not json')
+    assert.equal(expected, actual)
   })
 
 })
-
-function assert_fail_msg(expected, actual) {
-  return `
-      Expected: '${expected}'
-        to be found within:
-      Actual:   '${actual}'
-  `
-}
-
-function last_log_line() {
-  const
-    log_contents =
-      fs
-      .readFileSync(CT.vendor.electron_log.transports.file.findLogPath(), 'utf-8')
-      .split('\n')
-
-  return log_contents[log_contents.length - 2]
-}
