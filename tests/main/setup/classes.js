353405ca2d47303f5bb629b0fef3706cef11725d
Sean Lerner <sean@@smallcity.ca>
2018-03-13 18:03:33 -0500

    tests all classes

---
+++ tests/main/setup/classes.js
@@ -0,0 +1,17 @@
+require('../../test_helper')
+
+const classes = [ 'About', 'Account', 'Clipboard', 'Monitor', 'Config', 'Log', 'Logs', 'Menu', 'Notify', 'Prefs', 'Ready', 'Run', 'Reject', 'System', 'Teardown', 'Tray', 'TriggerDirectory', 'Triggers', 'Updater']
+
+const test_class = klass => {
+  const expected = 'function'
+  const actual   = typeof CT[klass]
+  assert.equal(expected, actual, `${klass}.js class setup failed.`)
+}
+
+const test_classes = () => {
+  classes.forEach(test_class)
+}
+
+describe('classes.js', function () {
+  it('all classes', test_classes)
+})
