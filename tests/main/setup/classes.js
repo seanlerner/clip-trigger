f50db5c6d0aac8f111db1de5fe4234c934a6e865
Sean Lerner <sean@@smallcity.ca>
2018-03-19 15:11:25 -0500

    cleans up classes test

--- tests/main/setup/classes.js
+++ tests/main/setup/classes.js
@@ -1,15 +1,34 @@
-const classes = [ 'About', 'Account', 'Clipboard', 'Monitor', 'Config', 'Log', 'Logs', 'Menu', 'Notify', 'Prefs', 'Ready', 'Run', 'Reject', 'System', 'Teardown', 'Tray', 'TriggerDirectory', 'Triggers', 'Updater']
-
 const test_class = klass => {
   const actual   = typeof CT[klass]
   const expected = 'function'
   assert.equal(actual, expected, `${klass}.js class setup failed.`)
 }
 
-const test_classes = () => {
-  classes.forEach(test_class)
-}
-
 describe('classes.js', function () {
-  it('all classes', test_classes)
+  it('all classes', () => {
+
+    const classes = [
+            'About',
+            'Account',
+            'Clipboard',
+            'Monitor',
+            'Config',
+            'Log',
+            'Logs',
+            'Menu',
+            'Notify',
+            'Prefs',
+            'Ready',
+            'Run',
+            'Reject',
+            'System',
+            'Teardown',
+            'Tray',
+            'TriggerDirectory',
+            'Triggers',
+            'Updater'
+          ]
+
+    classes.forEach(test_class)
+  })
 })
